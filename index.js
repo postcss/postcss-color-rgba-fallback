3324d29b2fbeeaa9e5c266e9552ac9ff017c1635
Stéphane Goetz <stephane.goetz@onigoetz.ch>
2021-05-10 21:04:26 +0200

    Update to PostCSS 8

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 /**
  * Module dependencies.
  */
-var postcss = require("postcss")
-var valueParser = require("postcss-value-parser")
-var rgbToHex = require("rgb-hex")
+const valueParser = require("postcss-value-parser")
+const rgbToHex = require("rgb-hex")
 
 /**
  * Calculate the color of a chanel
  * based upon two 0-255 colors and a 0-1 alpha value
  */
 function calcChannel(backgroundColor, foregroundColor, alpha) {
-  var value = backgroundColor + (foregroundColor - backgroundColor) * alpha
+  const value = backgroundColor + (foregroundColor - backgroundColor) * alpha
   return Math.round(value)
 }
 
@@ -40,10 +39,9 @@ function calculateRGB(backgroundColor, foregroundColor) {
 /**
  * PostCSS plugin to transform rgba() to hexadecimal
  */
-module.exports = postcss.plugin("postcss-color-rgba-fallback", function(options) {
-  options = options || {}
+module.exports = function(options = {}) {
 
-  var properties = options.properties || [
+  const properties = options.properties || [
     "background-color",
     "background",
     "color",
@@ -53,9 +51,9 @@ module.exports = postcss.plugin("postcss-color-rgba-fallback", function(options)
     "outline-color",
   ]
 
-  var backgroundColor = options.backgroundColor || null
+  const backgroundColor = options.backgroundColor || null
 
-  var oldie = options.oldie
+  let oldie = options.oldie
   if (oldie === true) {
     oldie = [
       "background-color",
@@ -66,75 +64,80 @@ module.exports = postcss.plugin("postcss-color-rgba-fallback", function(options)
     oldie = false
   }
 
-  return function(style) {
-    style.walkDecls(function(decl) {
-      if (!decl.value ||
-          decl.value.indexOf("rgba") === -1 ||
-          properties.indexOf(decl.prop) === -1
-      ) {
-        return
-      }
-
-      // if previous prop equals current prop
-      // no need fallback
-      if (
-        decl.prev() &&
-        decl.prev().prop === decl.prop
-      ) {
-        return
-      }
-
-      var hex
-      var alpha
-      var RGB
-      var value = valueParser(decl.value).walk(function(node) {
-        var nodes = node.nodes
-        if (node.type === "function" && node.value === "rgba") {
-          try {
-            alpha = parseFloat(nodes[6].value)
-            RGB = calculateRGB(backgroundColor, [
-              parseInt(nodes[0].value, 10),
-              parseInt(nodes[2].value, 10),
-              parseInt(nodes[4].value, 10),
-              alpha,
-            ])
-            hex = rgbToHex.apply(null, RGB)
-            node.type = "word"
-            node.value = "#" + hex
-          }
-          catch (e) {
+  return {
+    postcssPlugin: "postcss-color-rgba-fallback",
+    Once(root, {decl}) {
+      root.walkDecls(function(declaration) {
+        if (!declaration.value ||
+          declaration.value.indexOf("rgba") === -1 ||
+            properties.indexOf(declaration.prop) === -1
+        ) {
+          return
+        }
+  
+        // if previous prop equals current prop
+        // no need fallback
+        if (
+          declaration.prev() &&
+          declaration.prev().prop === declaration.prop
+        ) {
+          return
+        }
+  
+        let hex
+        let alpha
+        let RGB
+        const value = valueParser(declaration.value).walk(function(node) {
+          const nodes = node.nodes
+          if (node.type === "function" && node.value === "rgba") {
+            try {
+              alpha = parseFloat(nodes[6].value)
+              RGB = calculateRGB(backgroundColor, [
+                parseInt(nodes[0].value, 10),
+                parseInt(nodes[2].value, 10),
+                parseInt(nodes[4].value, 10),
+                alpha,
+              ])
+              hex = rgbToHex.apply(null, RGB)
+              node.type = "word"
+              node.value = "#" + hex
+            }
+            catch (e) {
+              return false
+            }
             return false
           }
-          return false
+        }).toString()
+  
+        if (value !== declaration.value) {
+          declaration.cloneBefore({value: value})
+  
+          if (
+            oldie && oldie.indexOf(declaration.prop) !== -1 &&
+            0 < alpha && alpha < 1
+          ) {
+            hex = "#" + Math.round(alpha * 255).toString(16) + hex
+            const ieFilter = [
+              "progid:DXImageTransform.Microsoft.gradient(startColorStr=",
+              hex,
+              ",endColorStr=",
+              hex,
+              ")",
+            ].join("")
+            const gteIE8 = decl({
+              prop: "-ms-filter", value: "\"" + ieFilter + "\"",
+            })
+            const ltIE8 = decl({
+              prop: "filter", value: ieFilter,
+            })
+  
+            declaration.parent.insertBefore(declaration, gteIE8)
+            declaration.parent.insertBefore(declaration, ltIE8)
+          }
         }
-      }).toString()
-
-      if (value !== decl.value) {
-        decl.cloneBefore({value: value})
-
-        if (
-          oldie && oldie.indexOf(decl.prop) !== -1 &&
-          0 < alpha && alpha < 1
-        ) {
-          hex = "#" + Math.round(alpha * 255).toString(16) + hex
-          var ieFilter = [
-            "progid:DXImageTransform.Microsoft.gradient(startColorStr=",
-            hex,
-            ",endColorStr=",
-            hex,
-            ")",
-          ].join("")
-          var gteIE8 = postcss.decl({
-            prop: "-ms-filter", value: "\"" + ieFilter + "\"",
-          })
-          var ltIE8 = postcss.decl({
-            prop: "filter", value: ieFilter,
-          })
-
-          decl.parent.insertBefore(decl, gteIE8)
-          decl.parent.insertBefore(decl, ltIE8)
-        }
-      }
-    })
+      })
+    },
   }
-})
+}
+
+module.exports.postcss = true
